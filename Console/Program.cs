f21fe8b81f44cc2cf378e900eca0b65e2b6da156
zedr0n <zedr0nre@gmail.com>
2016-02-19 23:22:51 +0000

    add console example

---
+++ Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+using SimpleInjector;
+using SimpleInjector.Extensions.ExecutionContextScoping;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var container = new Container();
+            container.Options.RegisterParameterConvention(new UrlConvention());
+
+            container.Options.DefaultScopedLifestyle = new ExecutionContextScopeLifestyle();
+            container.Register(typeof(IProvider<>), typeof(Provider<>), Lifestyle.Scoped);
+            container.Register<IWebString, WebString>();
+
+
+            container.RegisterConditional<IElementProvider, WebElementProvider>(c => container.GetCurrentExecutionContextScope() == null);
+            container.RegisterConditional<IElementProvider, ElementProvider>(Lifestyle.Scoped,
+                c => container.GetCurrentExecutionContextScope() != null);
+            //container.RegisterFunc<IElement, InboxMessage>();
+            //container.Register<ILoginService,SALogin>(Lifestyle.Scoped);
+
+            container.Verify();
+        }
+    }
+}
---
+++ Console/WebString.cs
@@ -0,0 +1,11 @@
+﻿namespace Lib
+{
+    public interface IWebString { }
+
+    public class WebString : IWebString
+    {
+        public WebString(IElementProvider elementProvider, string url)
+        {
+        }
+    }
+}
